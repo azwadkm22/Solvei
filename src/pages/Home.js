170138b81a1e17f3940db28edad34322324c694e
unknown <azwadkm22@gmail.com>
2023-04-08 15:02:59 +0600

    Fixed some styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,21 @@ import React from 'react'
 import FoldableContainer from '../components/FoldableContainer'
 import SideMenu from '../components/SideMenu'
 import Activity from '../components/Activity'
+import './styles/Home.css'
 function Home() {
   return (
     <div>
+        <div className='main-content'>
+
         <FoldableContainer semester="1st Year 1st Semester" />
-        <SideMenu title="Semester Top Contributors" content="Azwad"/>
         <FoldableContainer semester="1st Year 2nd Semester" />
+        </div>
+        <div className='side-content'>
+        <SideMenu title="Semester Top Contributors" content="Azwad" />
+
         <SideMenu title="Recent Activity" content={<Activity />} />
+        </div>
+        
     </div>
   )
 }
