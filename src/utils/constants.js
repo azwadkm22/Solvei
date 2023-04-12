e2cc4c13a60053d2c756b50a9636364c28c37f68
unknown <azwadkm22@gmail.com>
2023-04-13 05:17:19 +0600

    CSS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8080/api/";
+export const API_BASE_URL = "https://solvei-api.onrender.com/api/";
 export const AUTH = "auth/";
 export const REG = "register/"
 export const HOME = "course/";
