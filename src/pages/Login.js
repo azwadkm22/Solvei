4367617f0cceb45124cab0e3006ad9490a8fd145
unknown <azwadkm22@gmail.com>
2023-05-08 06:53:33 +0600

    Authentication Pages Fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import LoginForm from '../components/LoginForm'
-import "./styles/Auth.css"
 import { Link } from 'react-router-dom'
 
 function Login() {
   return (
     <div className='auth-center-container'>
-      <div className='auth-content'> 
+      <div className='auth-content logo-container'> 
         <img className='logo-content' src='https://i.imgur.com/qaZ4f4R.png'/>
       </div>
       <div className='auth-content'> 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import LoginForm from '../components/LoginForm'
-import "./styles/Auth.css"
 import RegisterForm from '../components/RegisterForm'
 import { Link } from 'react-router-dom'
 
@@ -8,7 +7,7 @@ import { Link } from 'react-router-dom'
 function Register () {
   return (
     <div className='auth-center-container'>
-      <div className='auth-content'>
+      <div className='auth-content logo-container'>
         <img className='logo-content' src='https://i.imgur.com/qaZ4f4R.png' />
       </div>
       <div className='auth-content'>
