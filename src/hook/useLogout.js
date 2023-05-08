e40d123c6f0696a4c631866ac6cb1e97e032a9a6
nashminn <nawar.nashmin@gmail.com>
2023-05-08 18:44:29 +0600

    logout works

---
+++ src/hook/useLogout.js
@@ -0,0 +1,17 @@
+import { useAuthContext } from "./useAuthContext"
+
+export const useLogout = ()=>{
+    const { dispatch } = useAuthContext()
+
+    const logout = ()=>{
+        console.log("inside logout")
+        // remove user from storage
+        localStorage.removeItem('user')
+
+        // dispatch logout action
+        dispatch({type: 'LOGOUT'})
+    };
+    console.log("type of logout: ", typeof(logout))
+
+    return logout
+}
